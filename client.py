f180ceb2dd9d6ca20e1d44570106d78840c55ef4
alberto-is <albertobarrais12@gmail.com>
2024-11-29 17:35:22 +0100

    chore: Add client and server files for TCP communication

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '192.168.1.136'
+PORT = 4670
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    client_socket.connect((HOST, PORT))
+    print(f"Conectado al servidor {HOST}:{PORT}...")
+
+    while True:
+        message = input("Cliente dice")
+        client_socket.sendall(message.encode())
+        print(f"Cliente dice: {message}")
+
+        data = client_socket.recv(1024)
+        print(f"Servidor responde: {data.decode()}")
+
+except ConnectionRefusedError:
+    print(f"No se pudo conectar al servidor en {HOST}:{PORT}")
+except KeyboardInterrupt:
+    print("\nCliente cerrado manualmente.")
+finally:
+    client_socket.close()
